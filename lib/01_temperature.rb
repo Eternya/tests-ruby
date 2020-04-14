fcfde14071b93fe7dc1586a580ac2c4c04fa6cd9
Eternya <53874831+Eternya@users.noreply.github.com>
2020-04-14 10:52:37 +0200

    Update 01_temperature.rb

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,9 +1,9 @@
    
 
-    def ftoc(temp.to_f - 32 ) × 5/9
-       
+    def ftoc(temp)
+       return ftoc(temp.to_f - 32 ) × 5/9
     end
-    def ctoc(temp × 9/5) + 32
-       
+    def ctoc(temp)
+       return ctoc(temp × 9/5) + 32
     end
 
